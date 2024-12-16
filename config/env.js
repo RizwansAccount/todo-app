46adda25f42169a93affaf2a2bb08e82c373ffc3
Rizsid <rizsid16@gmail.com>
2024-12-16 20:32:45 +0500

    changes

--- config/env.js
+++ config/env.js
@@ -1,6 +1,5 @@
 export const env = {
 	port: process.env.PORT || 5000,
 	nodeEnv: process.env.NODE_ENV || "development",
-	mongodbUri: process.env.DB_URI || "mongodb://127.0.0.1:27017/todo-app",
-	jwtSecret: process.env.JWT_SECRET || "my_temporary_secret",
+	mongodbUri: process.env.DB_URI || "mongodb://localhost:27017",
 };
